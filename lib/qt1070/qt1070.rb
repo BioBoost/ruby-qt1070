ede5effeba555ae504477736d477781d3c69fdb1
Sille Van Landschoot <info@sillevl.be>
2016-05-20 11:13:24 +0000

    update response format of register reads

--- lib/qt1070/qt1070.rb
+++ lib/qt1070/qt1070.rb
@@ -10,109 +10,114 @@ class Qt1070
  	end
 
   def chip_id
-    id = read 0x00
+    id = read_byte 0x00
     minor = id & 0x0F
     major = (id >> 4) & 0x0F
     {major: major, minor: minor}
   end
 
   def firmware_version
-    version = read 0x01
-    minor = id & 0x0F
-    major = (id >> 4) & 0x0F
+    version = read_byte 0x01
+    minor = version & 0x0F
+    major = (version >> 4) & 0x0F
     "#{major}.#{minor}"
   end
 
   def detection_status
-    status = read 0x02
+    status = read_byte 0x02
     return "Calibrate" if status & (1 << 7) != 0
     return "Touch" if status & (1 << 0) != 0
     return "Overflow" if status & (1 << 6) != 0
     "Undefined"
   end
 
-  def key_status
-    status_value = read 0x03
-    status = {}
-    8.times do |i|
-      status["key#{i}".to_sym] = status_value & (1 << i) != 0 ? "pressed" : "released"
+  def key_status key = nil
+    unless key.nil?
+        raise "Key index must be between 0 and 6" unless key.between? 0, 6
     end
-    status
-  end
-
-  def key_status key
-    raise "Key index must be between 0 and 6" unless key.between? 0, 6
-    status_value = read 0x03
-    status_value & (1 << key) != 0 ? "pressed" : "released"
+    status_value = read_byte 0x03
+    status = [] 
+    8.times do |i|
+      status[i] = status_value & (1 << i) != 0 ? "pressed" : "released"
+    end
+    key.nil? ? status : status[i]
   end
 
   def key_signal key
     raise "Key index must be between 0 and 6" unless key.between? 0, 6
     register = 0x04 + (key * 2)
-    signal = read register, 2
-    ((signal[1] * 256) + signal[0])
+    read_short register
   end
 
   def reference_data key
-    raise "Key index must be between 0 and 6" unless key.between? 0, 6
+    raise "Key index must b between 0 and 6" unless key.between? 0, 6
     register = 18 + (key * 2)
-    data = read register, 2
-    ((data[1] * 256) + data[0])
+    read_short register
   end
 
   #TODO write to this register
   def negative_threshold_level key
     raise "Key index must be between 0 and 6" unless key.between? 0, 6
-    read 32 + key
+    read_byte 32 + key
   end
 
   #TODO write to this register
   def adjacent_key_suppression_level key
     #TODO parse/check result
     raise "Key index must be between 0 and 6" unless key.between? 0, 6
-    read 39 + key
+    read_byte 39 + key
   end
 
   #TODO write to this register
   def detection_integrator_counter key
     #TODO parse/check result
     raise "Key index must be between 0 and 6" unless key.between? 0, 6
-    read 46 + key
+    read_byte 46 + key
   end
 
   #TODO write to this register
   def fo_mo_guard_no
     #TODO parse/check result
-    read 53
+    read_byte 53
   end
 
   #TODO write to this register
   def low_power_mode
     #TODO parse/check result
-    read 54
+    read_byte 54
   end
 
   #TODO write to this register
   def maximum_on_duration
     #TODO parse/check result
-    read 55
+    read_byte 55
   end
 
   #TODO write to this register
   def calibrate
     #TODO parse/check result
-    read 56
+    read_byte 56
   end
 
   #TODO write to this register
   def reset
     #TODO parse/check result
-    read 57
+    read_byte 57
   end
 
   private
   def read register, read_bytes = 1
     @i2c.read I2C_ADDRESS, read_bytes , register
+  end
+
+  def read_byte register
+    data = read register
+    data.unpack("C").first
+  end
+  
+  def read_short register
+    data = read register, 2
+    data.unpack("S_").first
   end
 end
 
--- test/test.rb
+++ test/test.rb
@@ -6,4 +6,10 @@ include Qt1070
 
 touch = Qt1070::Qt1070.new "/dev/i2c-1"
 
-puts "QT1070 state: #{touch.state.chr}"
+
+
+puts "Chip id: #{touch.chip_id.inspect}"
+puts "Firmware version: #{touch.firmware_version}"
+puts "Detection status: #{touch.detection_status}"
+puts "QT1070 state: #{touch.key_status}"
+puts "Key 1 signal: #{touch.key_signal 1}"
